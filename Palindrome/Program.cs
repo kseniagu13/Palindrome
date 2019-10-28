90f610a6bdb4bf20b1dc851c0b7deb08351129a6
Ksenia Gustsenko <kseniagustsenko@Ksenias-MacBook-Air.local>
2019-10-28 13:06:58 +0200

    Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Küsime nime
+            // Salvestame nime massiivi Char tüüpi
+            //Array.reverse
+            //teeme uus String, et saaks esialgse nime võrrelda Reverse nimega.
+            string name;
+            Console.WriteLine("Please enter a name: ");
+            name = Console.ReadLine();
+
+            char[] temp = name.ToCharArray();
+            Array.Reverse(temp);
+            string nameReverse = new string(temp);
+
+            if (name.ToLower() == nameReverse.ToLower())
+            {
+                Console.WriteLine("It is a Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Palindrome.");
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
